16b9abffd306449756e06c5fe8e43245d376c049
manel talbi <talbimanel20@gmail.com>
2022-01-08 11:02:58 +0100

    initial commit

---
+++ SimpleClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace simpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string car1Make;
+            string car1Model;
+            int car1Year;
+            string Car3Color;
+
+
+            string car2Make;
+            string car2Model;
+            int car2Year;
+            string Car3Color;
+
+            string car3Make;
+            string car3Model;
+            int car3Year;
+            string Car3Color;
+
+
+        }
+    }
+}
