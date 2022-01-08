de0233b277e61b57cfaa8b0791d012672d84e6b3
manel talbi <talbimanel20@gmail.com>
2022-01-08 11:30:32 +0100

    Create new instance from class Car

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            Car myCar = new Car();
+            myCar.Make = "oldsmobile";
+            myCar.Model = "cutlas supreme";
+            myCar.Year = 1986;
+            myCar.Color = "silver";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make, 
+                myCar.Model, 
+                myCar.Year,
+                myCar.Color);
+            Console.ReadLine();
+
+
 
 
         }
