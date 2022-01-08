5166da62da9405298b2105b39afc0b97a9e1a101
manel talbi <talbimanel20@gmail.com>
2022-01-08 11:10:54 +0100

    define class car

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -4,23 +4,18 @@
     {
         static void Main(string[] args)
         {
-            string car1Make;
-            string car1Model;
-            int car1Year;
-            string Car3Color;
-
-
-            string car2Make;
-            string car2Model;
-            int car2Year;
-            string Car3Color;
-
-            string car3Make;
-            string car3Model;
-            int car3Year;
-            string Car3Color;
+            
 
 
         }
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
     }
 }
