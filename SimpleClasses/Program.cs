d8666c526780a21776ad6762a41cd6a1b475d99d
manel talbi <talbimanel20@gmail.com>
2022-01-08 11:53:21 +0100

    create method outside a class

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -11,15 +11,27 @@
             myCar.Color = "silver";
 
             Console.WriteLine("{0} {1} {2} {3}",
-                myCar.Make, 
-                myCar.Model, 
+                myCar.Make,
+                myCar.Model,
                 myCar.Year,
                 myCar.Color);
+
+            decimal value = DetermineMarketValue(myCar);
+            Console.WriteLine("{0:C}" , value);
+
             Console.ReadLine();
 
 
 
 
+        }
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carVAlue = 100.0M;
+            // Someday i might look up the car
+            //online webservice to get more
+            //accurate value.
+            return carVAlue;
         }
     }
     class Car
